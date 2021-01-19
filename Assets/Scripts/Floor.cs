78143f64cb2765e793d6ffe4c741b3ec6df95869
randallard <ryan_dirks@waed.uscourts.gov>
2021-01-18 20:53:17 -0800

    Create Grid and Couple lObject
    Create Grid and Couple lObject

--- Assets/Scripts/Floor.cs
+++ Assets/Scripts/Floor.cs
@@ -5,10 +5,23 @@ using UnityEngine;
 public class Floor : MonoBehaviour
 {
     public bool tapMakesDancerOn = false;
+    public int squareCount = 1;
 
     // Start is called before the first frame update
     void Start()
     {
+        for (int i = 0; i < squareCount; i++)
+        {
+            GameObject square = Instantiate(Resources.Load("Grid"),
+                new Vector3(0,0,0),Quaternion.identity) as GameObject;
+            for (int dancerCount = 0; dancerCount < 8; dancerCount++)
+            {
+                GameObject newDancerLeft =
+                    Instantiate(Resources.Load("Dancer"),
+                        new Vector3(Random.Range(-1.5f, 1.5f), 1.3f, Random.Range(-1.5f, 1.5f)),
+                        Quaternion.identity) as GameObject;
+            }
+        }
     }
 
     void OnMouseDown()
